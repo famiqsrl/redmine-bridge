e6e5dadc8495ea0551c7ebc8c1f0093473894241
agroppa <agroppa@famiq.com.ar>
2026-02-19 13:34:05 -0300

    Cambios en requestContext

--- src/RequestContext.php
+++ src/RequestContext.php
@@ -6,18 +6,54 @@ namespace Famiq\RedmineBridge;
 
 final class RequestContext
 {
+    public string $correlationId;
+
+    /**
+     * Login/identificador del usuario para X-Redmine-Switch-User.
+     * (En Redmine suele ser el "login", no el email completo)
+     */
+    public ?string $idUsuario;
+
+    public ?string $emailUsuario;
+    public ?string $nombreUsuario;
+    public ?string $apellidoUsuario;
+
+    /**
+     * @deprecated Usar $idUsuario. Se mantiene por compat con callers que hacen:
+     *           new RequestContext(correlationId: ..., switchUser: ...)
+     */
+    public ?string $switchUser;
+
     public function __construct(
-        public string $correlationId,
-        public ?string $idUsuario = null,
-        public ?string $emailUsuario = null,
-        public ?string $nombreUsuario = null,
-        public ?string $apellidoUsuario = null,
-        public ?string $switchUser = null,
+        string $correlationId,
+        ?string $idUsuario = null,
+        ?string $emailUsuario = null,
+        ?string $nombreUsuario = null,
+        ?string $apellidoUsuario = null,
+        ?string $switchUser = null,
     ) {
+        $this->correlationId = $correlationId;
+
+        // Compat:
+        // - llamadas viejas: new RequestContext($trace, $login) => $idUsuario OK
+        // - llamadas nuevas: new RequestContext(correlationId: ..., switchUser: ...) => lo mapeamos a idUsuario
+        $this->idUsuario = $idUsuario ?? $switchUser;
+
+        $this->switchUser = $switchUser ?? $idUsuario;
+
+        $this->emailUsuario = $emailUsuario;
+        $this->nombreUsuario = $nombreUsuario;
+        $this->apellidoUsuario = $apellidoUsuario;
     }
 
     public static function generate(): self
     {
-        return new self(bin2hex(random_bytes(16)));
+        try {
+            $cid = bin2hex(random_bytes(16));
+        } catch (\Throwable) {
+            $cid = uniqid('cid_', true);
+        }
+
+        return new self($cid);
     }
 }
